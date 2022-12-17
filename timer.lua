678ff86fad41ed912baa1246757dc177896c34f7
HollowedOutMods <112557613+HollowedOutMods@users.noreply.github.com>
2022-12-17 00:01:23 +0000

    Update timer.lua

--- timer.lua
+++ timer.lua
@@ -1,7 +1,14 @@
 local ultimaroom = game.ReplicatedStorage.GameData.LatestRoom.Value
 
+plr.Health = game.Players.LocalPlayer.Character.Humanoid.Health
+
 while true do
 wait(1)
-game.Players.LocalPlayer.Character.Humanoid.Health -= 5
+plr.Health = plr.Health - 5
 if ultimaroom ~= game.ReplicatedStorage.GameData.LatestRoom.Value then break end
+if game.Players.LocalPlayer.Character.Humanoid.Health >= 0 then 
+    local death = game.Players.LocalPlayer.PlayerGui:WaitForChild("MainUI"):WaitForChild("Statistics"):WaitForChild("Death")
+    -- use death.Text = ""
+    death.Text = "Died to staying in a room for too long"
+end
 end
